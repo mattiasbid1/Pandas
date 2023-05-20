5c51b0c66a98e50ecef8744776d70e5b61ddc2ea
Mattias Björk <mattias.bjork.id1@gmail.com>
2023-05-20 21:38:31 +0200

    updates

--- main.py
+++ main.py
@@ -1,2 +1,44 @@
-# Testing panda
+# import csv
+# # with open("weather_data.csv") as file:
+# #     weather_list_raw = file.readlines()
+# #     weather_list = [line.strip() for line in weather_list_raw]
+# #     for weather in weather_list:
+# #         print(weather)
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1].isnumeric():
+#             temperatures.append(int(row[1]))
+#     print([value for value in temperatures])
+#     print(temperatures)
 
+import pandas
+
+# data = pandas.read_csv("weather_data.csv")
+# print(data["temp"])
+
+# data_dict = data.to_dict()
+# print(data_dict)
+# #
+# # templist = data["temp"].to_list()
+# # print(round(sum(templist) / len(templist)))
+#
+# print(data.temp.max())
+#
+# print(data[data.temp == data.temp.max()])
+#
+# monday = data[data.day == "Monday"]
+# monday_temp = monday.temp * 9 / 5 + 32
+# print(monday_temp)
+
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65],
+}
+
+data = pandas.DataFrame(data_dict)
+
+print(data)
+data.to_csv("new_data.csv")
